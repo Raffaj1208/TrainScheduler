4c8e8abbcbb2accb7eca2f747e0d55e09ed3afe4
Raffaj1208 <61484778+Raffaj1208@users.noreply.github.com>
2020-05-19 14:44:55 -0700

    added firebase cdn. created the apiKey variable

---
+++ Assets/Javascript/app.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyB7KtHLRHH1j6TP0OT2YiRkVGh_8cyxrrA",
+    authDomain: "trainscheduler-ba1c3.firebaseapp.com",
+    databaseURL: "https://trainscheduler-ba1c3.firebaseio.com",
+    projectId: "trainscheduler-ba1c3",
+    storageBucket: "trainscheduler-ba1c3.appspot.com",
+    messagingSenderId: "319855585835",
+    appId: "1:319855585835:web:c6093e236d664c7b12186e",
+    measurementId: "G-XZ0XJBDY5G"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
