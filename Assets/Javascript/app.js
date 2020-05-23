a8777b32805a082b2cb896d3bf15fc142d8ecb4b
Raffaj1208 <61484778+Raffaj1208@users.noreply.github.com>
2020-05-22 18:12:08 -0700

    ..

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -1,14 +1,27 @@
-  //.. Init
+//.. Your web app's Firebase configuration
+let firebaseConfig = {
+  apiKey: "AIzaSyB7KtHLRHH1j6TP0OT2YiRkVGh_8cyxrrA",
+  authDomain: "trainscheduler-ba1c3.firebaseapp.com",
+  databaseURL: "https://trainscheduler-ba1c3.firebaseio.com",
+  projectId: "trainscheduler-ba1c3",
+  storageBucket: "trainscheduler-ba1c3.appspot.com",
+  messagingSenderId: "319855585835",
+  appId: "1:319855585835:web:c6093e236d664c7b12186e",
+  measurementId: "G-XZ0XJBDY5G"
+};
+  //.. Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  //..
+  let dataRef = firebase.database();
+  //
+
   let name = "testingName";
   let destination = "testingDest";
   let FirstTrainTime = "testingTime";
   let frequency = "testingFreq";
-$("#displayName").text(name);
-$("#displayDestination").text(destination);
-$("#displayFirstTrain").text(FirstTrainTime);
-$("#displayFrequency").text(frequency);
-
-//.. Function on Submit button CLick
+  
+  //.. Function on Submit button CLick
 $("#submit").on("click", function(){
   event.preventDefault();
 //.. Name ..//
@@ -23,22 +36,39 @@ $("#displayFirstTrain").text(FirstTrainTime);
   //.. Frequency ..//
 frequency = $("#frequency-input").val().trim();
 $("#displayFrequency").text(frequency);
+
+//
+dataRef.ref().push({
+name: name,
+destination: destination,
+FirstTrainTime: FirstTrainTime,
+frequency: frequency,
+dataAdded: firebase.database.ServerValue.TIMESTAMP
+});
 });
 
-//.. Your web app's Firebase configuration
-let firebaseConfig = {
-    apiKey: "AIzaSyB7KtHLRHH1j6TP0OT2YiRkVGh_8cyxrrA",
-    authDomain: "trainscheduler-ba1c3.firebaseapp.com",
-    databaseURL: "https://trainscheduler-ba1c3.firebaseio.com",
-    projectId: "trainscheduler-ba1c3",
-    storageBucket: "trainscheduler-ba1c3.appspot.com",
-    messagingSenderId: "319855585835",
-    appId: "1:319855585835:web:c6093e236d664c7b12186e",
-    measurementId: "G-XZ0XJBDY5G"
-  };
-  //.. Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+//
+dataRef().on("child_added", function(childSnapshot) {
+  $("#full-member-list").append("<div class='mainDisplay'><td class='infoName'> " +
+  childSnapshot.val().name +
+  " </td><td class='infoDest'> " + childSnapshot.val().destination +
+  " </td><td class='infoFirst'> " + childSnapshot.val().FirstTrainTime +
+  " </td><td class='infoFreq'> " + childSnapshot.val().frequency +
+  " </td></div>");
+
+});
+//..
+database.ref().on("child_added", function(snapshot){
+  let snap = snapshot.val();
+  console.log(snap.name);
+  console.log(snap.destination);
+  console.log(snap.FirstTrainTime);
+  console.log(frequency);
 
   //..
-  let dataBase = firebase.database();
+  $("#displayName").text(snap.name);
+  $("#displayDestination").text(snap.destination);
+  $("#displayFirstTrain").text(snap.FirstTrainTime);
+  $("#displayFrequency").text(snap.frequency);
+  //
+});
